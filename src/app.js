eb3b4ba887908ecb09d01e31a4b1f3064043d9c3
Zohaib Qaiser <zohaibqaiser55@gmail.com>
2020-04-22 03:32:42 +0000

    here i have configured an package 'prettier' that will be used to reformat my code

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from 'express'
 const app = express()
 
-app.get('/',(req, res) => res.send('it works ig yaay'))
+app.get('/', (req, res) => res.send('it works ig yaay'))
 app.listen(8080, () => console.log('service is running'))
