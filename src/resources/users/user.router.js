00c2ccea3306894fcfc9f69bea788f977d982af0
Zohaib Qaiser <zohaibqaiser55@gmail.com>
2020-04-24 18:29:44 +0000

    moving to visual studio

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import userController from './user.controller'
 export const userRouter = express.Router()
 
 userRouter.get('/', (req, res) => res.send('User List'))
+userRouter.post('/',userController.createUser)
+
 userRouter.get('/:id', (req, res) => {
     res.send('User id is:'+ req.params.id)
 })
