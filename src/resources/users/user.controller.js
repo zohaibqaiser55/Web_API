b309acd3edad963627bfab32dca59941e5bca407
Zohaib Qaiser <zohaibqaiser55@gmail.com>
2020-04-25 07:06:06 +0000

    here i have added the Get And Post functionality of the CRUD

--- src/resources/users/user.controller.js
+++ src/resources/users/user.controller.js
@@ -6,14 +6,23 @@ import pick from 'lodash.pick'
 const userController ={
  async createUser(req, res) {
     try{
-
-    let user = new User(pick(req.body, ['email','password']))
+        let user = new User(pick(req.body, ['email','password']))
         await user.save()
-        res.send(user)
+        res.status(201).send(user)
+    }catch(error){
+        res.status(400).send(error)}
+ },
+ 
+ 
+ async getUsers(req, res){
+     try{
+        const result = await User.find().sort('createdAt')
+        res.status(200).send(result)
     }catch(error){
         res.status(400).send(error)
     }
- }  
+ },
+
 }
 
 
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -2,8 +2,11 @@ import express from 'express'
 import userController from './user.controller'
 export const userRouter = express.Router()
 
-userRouter.get('/', (req, res) => res.send('User List'))
-userRouter.post('/',userController.createUser)
+/**Routes for Crude fuctionality */
+userRoute
+.route('/')
+.get(userController.getUsers)
+.post(userController.createUser)
 
 userRouter.get('/:id', (req, res) => {
     res.send('User id is:'+ req.params.id)
